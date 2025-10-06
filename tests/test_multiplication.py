8b13adf4484a38c647a3ed08d11217eafa93ff1a
ralph-ie <colmenra@kean.edu>
2025-10-05 21:51:33 -0400

    Fixed docstrings and updated tests

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -4,6 +4,5 @@ from app.calculator import Calculator
 
 def test_multiplication():
     """Multiply two numbers"""
-
     result = Calculator.multiplication(val1=5, val2=5)
     assert result == 25, "The multiplication function returns the wrong value"
